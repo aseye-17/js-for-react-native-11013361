fbd885e36c6656bd3c6d5c126ef30eb5f6bc7bd2
Fulbert Aseye Kemeh <Fakemeh@st.ug.gh.edu>
2024-05-19 08:09:46 -0400

    second coimmit

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -13,3 +13,22 @@ function processArray(numbers) {
 const inputNumbers = [1, 2, 3, 4, 5];
 const processedNumbers = processArray(inputNumbers);
 console.log(processedNumbers); // [3, 4, 9, 16, 15]
+
+
+
+
+function formatArrayStrings(strings, processedNumbers) {
+    return strings.map((str, index) => {
+        if (processedNumbers[index] % 2 === 0) {
+            // Even number: capitalize the string
+            return str.toUpperCase();
+        } else {
+            // Odd number: convert the string to lowercase
+            return str.toLowerCase();
+        }
+    });
+}
+
+const inputStrings = ['Apple', 'Banana', 'Cherry', 'Date', 'Fig'];
+const formattedStrings = formatArrayStrings(inputStrings, processedNumbers);
+console.log(formattedStrings); // ["APPLE", "banana", "CHERRY", "date", "fig"]
